dbcf770017597ee9fe91978768b541dc664b53af
Tayhana Marçal Rangel da Fonseca <tayhanarangel@gmail.com>
2024-06-27 20:25:16 -0300

    UPDATE: extract and load

--- README.md
+++ README.md
@@ -2,17 +2,20 @@
 
 * Criação do repositório no GitHub
 * Criação do ambiente virtual
-    `python -m venv .venv`
 
+    `python -m venv .venv`
     `source .venv/Scripts/activate`
 
 * Instalar bibliotecas
-    requirements.txt
+
+> requirements.txt
 
 * Criar estrutura de pastas do projeto
-- src (código do projeto)
+> src (código do projeto)
     Extract com python
     Absorver dados da API
-- app (dashboard)
-- dbt (criação de pasta automática)
-- Cloud Render > PostgreSQL
+> app (dashboard)
+> dbt (criação de pasta automática)
+
+* Criação de banco no Cloud
+> Cloud Render > PostgreSQL
--- src/extract_load.py
+++ src/extract_load.py
@@ -8,9 +8,22 @@ import os #é usada para acessar variáveis de ambiente ou manipular o sistema d
 # import variáveis de ambiente
 commodities = ['CL=F', 'GC=F', 'SI=F']
 
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
 #Extract
 def search_commodities(symbol, period='5d', interval='1d'):
-    ticker = yf.Ticker('CL=F')
+    ticker = yf.Ticker(symbol)
     data = ticker.history(period=period, interval=interval)[['Close']]
     data['symbol'] = symbol
     return data
@@ -24,9 +37,11 @@ def search_all_commodities(commodities):
     return pd.concat(all_data)
 
 #Save
-
+def save_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
     
 #Load
 if __name__ == "__main__":
     concat_data = search_all_commodities(commodities)
-    print(concat_data) 
+    # print(concat_data) debug
+    save_postgres(concat_data, schema='public')
